09b3d854ba61560ab73bc257a30c52fe5f64fdb1
lypmoura <lypekjkj@icloud.com>
2025-03-26 03:17:47 -0300

    arrumei

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
